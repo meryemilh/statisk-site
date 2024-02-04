2b2cbd519bc06c3b936700be72308d83621f47ef
meryem <meil0001@stud.kea.dk>
2024-02-04 01:12:46 +0100

    produkter v1

---
+++ js/product.js
@@ -0,0 +1,10 @@
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
+  document.querySelector(".purchaseBox .brand").textContent = product.brandname;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}
