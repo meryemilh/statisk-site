8f4ce9a38c20238287aaefc98591c7a68063ed82
meryem <meil0001@stud.kea.dk>
2024-02-07 19:59:25 +0100

    menu og meget mere

--- js/product.js
+++ js/product.js
@@ -1,10 +1,24 @@
-fetch("https://kea-alt-del.dk/t7/api/products/1525")
-  .then((response) => response.json())
-  .then((data) => showProduct(data));
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const url = `https://kea-alt-del.dk/t7/api/products${id}`;
+
+const parent = document.querySelector("section");
+
+function getProduct() {
+  fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+    .then((response) => response.json())
+    .then((data) => showProduct(data));
+}
 
 function showProduct(product) {
   console.log(product);
+
+  const purchaseBox = document.querySelector(".purchaseBox");
+
   document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
   document.querySelector(".purchaseBox .brand").textContent = product.brandname;
-  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  ocument.querySelector("img").alt = product.productdisplayname;
 }
+
+getProduct();
---
+++ js/productlist.js
@@ -0,0 +1,30 @@
+fetch("https://kea-alt-del.dk/t7/api/products?limit=10")
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  //looper og kalder showProduct ental
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  // console.log(product);
+  //fange template
+  const template = document.querySelector("#smallProductTemplate").content;
+  //lave en kopi
+  const copy = template.cloneNode(true);
+  // ændre indhold
+  copy.querySelector("h3").textContent = product.productdisplayname;
+
+  copy.querySelector(".product-img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  copy.querySelector(".product-img").alt = product.productdisplayname;
+
+  if (product.soldout) {
+    //produktet er udsolgt
+    copy.querySelector("article").classList.add("soldOut");
+  }
+
+  copy.querySelector(".read-more").setAttribute("href", `produkt.html?id=${product.id}`);
+  //appende
+  document.querySelector("main").appendChild(copy);
+}
